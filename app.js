1b01e4e13862358851a1dd9d21b773ff6a0de005
Sohail Iran <aratavalga@gmail.com>
2023-09-27 20:46:40 -0500

    definition

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const textTitle = document.getElementById('father');
 
-textTitle.innerHTML = `<h1>Dom Traversing</h1>`;
+textTitle.innerHTML = `<h1>Dom Traversing</h1><p>Sirve para darle al DOM varios recolectar varios items en cada padre selector,<br>no obstante solo devuelve objetos en consola.</p>`;
